061ca183d9c9351c4498004232857e1b32ad2319
Deep <1023509177@qq.com>
2020-01-06 09:32:36 +0800

    Initial commit

---
+++ Inc/mpu6050.h
@@ -0,0 +1,9 @@
+//
+// Created by mayn on 2020/1/3 0003.
+//
+
+#ifndef HELLOSTM_MPU6050_H
+#define HELLOSTM_MPU6050_H
+
+
+#endif //HELLOSTM_MPU6050_H
---
+++ Src/mpu6050.c
@@ -0,0 +1,5 @@
+//
+// Created by mayn on 2020/1/3 0003.
+//
+
+#include "mpu6050.h"
